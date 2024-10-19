e38ef933aac30d760aaa7f292aedad0b77b084fa
ShambhuThapa <shambhu.thapa765@gmail.com>
2024-10-19 10:56:58 +0545

    tailwind and shadcn setup

--- blogs-supabase/src/App.tsx
+++ blogs-supabase/src/App.tsx
@@ -1,13 +1,16 @@
 
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
 
   return (
     <>
-      <h1>Blogs </h1>
+      <h1 className='italic text-3xl text-center'>Blogs </h1>
+      <Button variant="outline">Button</Button>
+
     </>
   )
-}
+} 
 
 export default App
